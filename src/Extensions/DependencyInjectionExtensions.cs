223f4e9f061402b73d87b3c86b0ffafdc6c0bf13
sunriseyuen <tiptoes@foxmail.com>
2022-08-17 17:57:51 +0800

    Update DependencyInjectionExtensions.cs

--- src/Extensions/DependencyInjectionExtensions.cs
+++ src/Extensions/DependencyInjectionExtensions.cs
@@ -2,7 +2,6 @@ using Elsa.Features.Services;
 using Elsa.Persistence.Common.Entities;
 using Elsa.Workflows.VoltePersistence.Features;
 using Elsa.Workflows.VoltePersistence.Implementations;
-using Elsa.Workflows.VoltePersistence.Implementations;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Volte.Data.Dapper;
@@ -11,11 +10,19 @@ namespace Elsa.Workflows.VoltePersistence.Extensions;
 
 public static class DependencyInjectionExtensions
 {
-    public static IModule UseWorkflowVoltePersistence(this IModule module, Action<WorkflowPersistenceFeature>? configure = default )
+
+    public static IModule UseWorkflowVoltePersistence(this IModule module, Action<WorkflowPersistenceFeature>? configure = default)
     {
         module.Configure(configure);
         return module;
     }
+
+    public static WorkflowPersistenceFeature UseWorkflowPersistence(this WorkflowPersistenceFeature feature, Action<WorkflowPersistenceFeature>? configure = default)
+    {
+        feature.Module.Configure(configure);
+        return feature;
+    }
+   
     public static IServiceCollection AddVolteStore<TEntity, TStore>(this IServiceCollection services) where TEntity : Entity where TStore : class
     {
         services.AddTransient<IDbContext, DbContext>();
